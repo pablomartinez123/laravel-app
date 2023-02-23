d7231be6245854b8a937e99772186f9062750857
Pablo Sebastian Martinez <pablomartinez123@hotmail.com>
2023-02-23 12:11:40 -0300

    Fix AccountUserPolicy

--- app/Policies/AccountUserPolicy.php
+++ app/Policies/AccountUserPolicy.php
@@ -13,7 +13,7 @@ class AccountUserPolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -21,7 +21,7 @@ class AccountUserPolicy
      */
     public function view(User $user, AccountUser $accountUser): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -29,7 +29,7 @@ class AccountUserPolicy
      */
     public function create(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -37,7 +37,7 @@ class AccountUserPolicy
      */
     public function update(User $user, AccountUser $accountUser): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -45,7 +45,7 @@ class AccountUserPolicy
      */
     public function delete(User $user, AccountUser $accountUser): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -53,7 +53,7 @@ class AccountUserPolicy
      */
     public function restore(User $user, AccountUser $accountUser): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -61,6 +61,6 @@ class AccountUserPolicy
      */
     public function forceDelete(User $user, AccountUser $accountUser): bool
     {
-        //
+        return true;
     }
 }
